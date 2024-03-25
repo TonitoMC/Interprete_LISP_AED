2bf5c98a3da762f81e2e67ce3419e2e2b48e03c4
adrianlop19 <110709904+adrianlop19@users.noreply.github.com>
2024-03-24 19:46:41 -0600

    Clase PredicatesTest
    implementación clase PredicatesTest

---
+++ src/test/java/uvg/edu/gt/PredicatesTest.java
@@ -0,0 +1,22 @@
+package uvg.edu.gt;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PredicatesTest extends TestCase {
+
+    public void testEval() {
+        Predicates pred = new Predicates();
+        String[] inputArrInc = {"(", "<", "1", "2", "3", "4", ")"};
+        String[] inputArrDec = {"(", ">", "4", "3", "2", "1", ")"};
+        String[] inputArrEq = {"(", "=", "1", "1", "1", ")"};
+        ArrayList<String> inputListInc = new ArrayList<>(Arrays.asList(inputArrInc));
+        ArrayList<String> inputListDec = new ArrayList<>(Arrays.asList(inputArrDec));
+        ArrayList<String> inputListEq = new ArrayList<>(Arrays.asList(inputArrEq));
+        assertTrue(pred.eval(inputListInc));
+        assertTrue(pred.eval(inputListDec));
+        assertTrue(pred.eval(inputListEq));
+    }
+}
