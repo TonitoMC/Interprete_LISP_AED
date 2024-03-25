1ca478fdc342d3aed4202b41454deb99ba8ed227
adrianlop19 <110709904+adrianlop19@users.noreply.github.com>
2024-03-24 19:39:40 -0600

    Update Predicates.java
    Implementacion clase predicates

--- src/main/java/uvg/edu/gt/Predicates.java
+++ src/main/java/uvg/edu/gt/Predicates.java
@@ -1,6 +1,7 @@
 package uvg.edu.gt;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Esta clase se encarga de evaluar predicados
@@ -10,7 +11,39 @@ import java.util.ArrayList;
  * @since 13-03-2024
  */
 public class Predicates {
-    public boolean eval(ArrayList<String> input){
-        return true;
+    public boolean eval(ArrayList<String> input) {
+        String operator = input.get(1);
+        List<String> subList = input.subList(2, input.size() - 1);
+        ArrayList<String> operatorList = new ArrayList<>(subList);
+        switch (operator) {
+            case "<":
+                for (int i = 1; i < operatorList.size(); i++) {
+                    double currentOperator = Double.parseDouble(operatorList.get(i));
+                    double previousOperator = Double.parseDouble(operatorList.get(i));
+                    if (previousOperator > currentOperator) {
+                        return false;
+                    }
+                }
+                return true;
+            case ">":
+                for (int i = 1; i < operatorList.size(); i++) {
+                    double currentOperator = Double.parseDouble(operatorList.get(i));
+                    double previousOperator = Double.parseDouble(operatorList.get(i));
+                    if (previousOperator < currentOperator) {
+                        return false;
+                    }
+                }
+                return true;
+            case "=":
+                for (int i = 1; i < operatorList.size(); i++) {
+                    double currentOperator = Double.parseDouble(operatorList.get(i));
+                    double previousOperator = Double.parseDouble(operatorList.get(i));
+                    if (previousOperator != currentOperator) {
+                        return false;
+                    }
+                }
+                return true;
+        }
+        return false;
     }
 }
